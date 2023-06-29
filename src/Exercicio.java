4ac75e24f94daad773efa252c5376a16e4591b52
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-06-29 20:12:42 -0300

    Exercicios

---
+++ src/Exercicio.java
@@ -0,0 +1,34 @@
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Procucts:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
+		
+		System.out.printf("Record: %d year old, code %d and gender: %s%n%n", age, code, gender);
+		
+		System.out.printf("Measue with eight decimal place: %s%n", measure);
+		System.out.printf("Rouded (three decimal place): %.f2%n", measure);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
