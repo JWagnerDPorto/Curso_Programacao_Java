1557c5a72cc006baada6c5bc679ba9c1647831f5
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-06-30 21:15:17 -0300

    Exemplo2

--- src/ExemplosAula.java
+++ src/ExemplosAula.java
@@ -4,7 +4,8 @@ public class ExemplosAula {
 	public static void main (String[] args) {
 		
 		
-		int x, y;
+		int x;
+		double y;
 		
 		x = 5;
 		y = 2 * x;
@@ -15,17 +16,5 @@ public class ExemplosAula {
 		
 	}
 
-	public static void Exemplo2 (String[] args) {
-		
-	
-		int x, y;
-		
-		x = 5;
-		y = 2 * x;
-		
-		System.out.println(x);
-		System.out.println(y);
-		
-		
-	}
+
 }
