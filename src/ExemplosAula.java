85174ba7ac8c38b04577daa2827153dc0033f01a
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-04 20:57:01 -0300

    Aula 19

--- src/ExemplosAula.java
+++ src/ExemplosAula.java
@@ -2,27 +2,30 @@ import java.util.Scanner;
 
 public class ExemplosAula {
 
-	public static void main (String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		String x;
-		int y;
-		double z;
-		
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
-		
-		System.out.println("Dados digitados: "); //Atalho (sysout + ctrl + space)
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		
-		sc.close();
-		
-	}
+	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
+		int x;
+		String s1, s2, s3;
+
+		x = sc.nextInt();
+		sc.nextLine();
+		s1 = sc.nextLine(); // Ler texto até a quebra de linha.
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("DADOS DIGITADOS: ");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+		// Ctrl+ A (seleciona tudo)
+		// Delete (para deletar)
+		// Ctrl + Shift + F (auto identação)
+
+		sc.close();
+	}
 
 }
