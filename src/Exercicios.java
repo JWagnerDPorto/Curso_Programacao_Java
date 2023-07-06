7802726977eea52399e6b7f94cfaf2a86c5b0b50
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-05 21:05:26 -0300

    Exercicio Aula 21

--- src/Exercicios.java
+++ src/Exercicios.java
@@ -1,32 +1,24 @@
-
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicios {
 
 	public static void main(String[] args) {
 
-		String product1 = "Computer";
-		String product2 = "Office desk";
-
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
-
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
-
-		System.out.println("Procucts:");
-		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
-
-		System.out.printf("Record: %d year old, code %d and gender: %s%n%n", age, code, gender);
-
-		System.out.printf("Measue with eight decimal place: %f%n", measure);
-		System.out.printf("Rouded (three decimal place): %.3f%n", measure);
 		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f", measure);
-
+		Scanner sc = new Scanner(System.in); //Atalho para inportação Ctrl + Shift + O
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento; //Atalho para auto completar as variaveis.
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", preco);
+		
+		sc.close();
 	}
 
 }
