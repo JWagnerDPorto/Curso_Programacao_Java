bacf6325cb7bca954418949d14274e13fc2aec48
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-27 21:20:47 -0300

    27/07

--- src/Exercicios.java
+++ src/Exercicios.java
@@ -6,18 +6,18 @@ public class Exercicios {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in); //Atalho para inportação Ctrl + Shift + O
-		
+		Scanner sc = new Scanner(System.in); // Atalho para inportação Ctrl + Shift + O
+
 		double largura = sc.nextDouble();
 		double comprimento = sc.nextDouble();
 		double metroQuadrado = sc.nextDouble();
-		
-		double area = largura * comprimento; //Atalho para auto completar as variaveis.
+
+		double area = largura * comprimento; // Atalho para auto completar as variaveis.
 		double preco = area * metroQuadrado;
-		
+
 		System.out.printf("AREA = %.2f%n", area);
 		System.out.printf("PRECO = %.2f%n", preco);
-		
+
 		sc.close();
 	}
 
